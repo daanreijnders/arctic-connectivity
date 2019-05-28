612e24256aa11fb0ec43466f782eb59b2a60c8f9
Daan Reijnders <daanreijnders@outlook.com>
2019-05-28 22:50:05 +0200

    working animation. Ready for test run on gemini

--- EEZ/plot.py
+++ EEZ/plot.py
@@ -1,11 +1,16 @@
 """This package contains specific tools I use for my research project."""
 import numpy as np
 import xarray as xr
+import pickle
+
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
 import cartopy as cart
 import cartopy.crs as ccrs
 from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+
 from parcels import (grid, Field, FieldSet, ParticleSet,
                      ErrorCode, ParticleFile, Variable, plotTrajectoriesFile)
 
@@ -46,8 +51,6 @@ def show(inputfield, trajectoryFile=None, particleDensity=False, \
         gl.ylabels_right = False
         gl.xformatter = LONGITUDE_FORMATTER
         gl.yformatter = LATITUDE_FORMATTER
-    
-
     
     # Trajectories
     if trajectoryFile != None:
@@ -131,24 +134,41 @@ def show(inputfield, trajectoryFile=None, particleDensity=False, \
     plt.show()
 
 
-class animation:
-    def EEZ_particles(pfile, fps=24):
+class particleAnimation:
+    def EEZ_particles(pfile, EEZ_ds, bleed=3, polar=False, nbar=False, eezIDmap=None, barlength=100, fps=24):
         # Load arrays from file
         lon = np.ma.filled(pfile.variables['lon'], np.nan)
         lat = np.ma.filled(pfile.variables['lat'], np.nan)
         time = np.ma.filled(pfile.variables['time'], np.nan)
         EEZ_evol = np.ma.filled(pfile.variables['z'], np.nan)
         mesh = pfile.attrs['parcels_mesh'] if 'parcels_mesh' in pfile.attrs else 'spherical'
-        pfile.close()
-
+        
+        minlon = np.amin(pfile.variables['lon'])
+        minlat = np.amin(pfile.variables['lat'])
+        maxlon = np.amax(pfile.variables['lon'])
+        maxlat = np.amax(pfile.variables['lat'])
+        if polar:
+            extent = (0, 360, minlat-bleed, 90)
+        else:
+            extent = (max(minlon-bleed, -180), min(maxlon+bleed, 180), max(minlat-bleed, -90), min(maxlat+bleed, 90))
+        
+        if polar:
+            map_crs = ccrs.NorthPolarStereo(central_longitude=0.0, globe=None)
+        else:
+            map_crs = ccrs.PlateCarree()
         # Create figure
         fig     = plt.figure(figsize=(9,5))
         particle_map = plt.subplot(projection=map_crs)   
 
         # Add background for land plot
-        particle_map.set_extent((-10,20,50,80), crs=map_crs)
+        particle_map.set_extent(extent, crs=map_crs)
         particle_map.coastlines()
         particle_map.add_feature(cart.feature.LAND, zorder=5, edgecolor='k')
+        
+        EEZ = EEZ_ds['EEZ'][:,:][0]
+        EEZ_lats = EEZ_ds['latitude'].data
+        EEZ_lons = EEZ_ds['longitude'].data
+        
         particle_map.pcolormesh(EEZ_lons, EEZ_lats, EEZ, transform=map_crs, cmap='Set3', zorder=1)
         gl = particle_map.gridlines(crs=map_crs, linestyle='--', draw_labels = True)
         gl.xlabels_top = False
@@ -164,45 +184,58 @@ class animation:
                 plottimes = plottimes[~np.isnan(plottimes)]
             except:
                 pass
+        print('Start and end plottimes:', plottimes[0], plottimes[-1])
         currtime = time == plottimes[0]
 
         scat   = particle_map.scatter(lon[currtime], lat[currtime], s=20, color='k', transform=ccrs.Geodetic())
+        
+        if nbar:
+            uniques, counts = np.unique(np.where(np.isnan(pfile['EEZ'].data), -1, pfile['EEZ'].data), return_counts=True) # NaNs become -1
+            EEZoccurenceDict = dict(zip(uniques, counts))
+            EEZoccurenceDict.pop(-1)
+            plotEEZbars = []
+            while len(EEZoccurenceDict) >= nbar and len(plotEEZbars) < nbar:
+                plotEEZbars.append(max(EEZoccurenceDict, key=EEZoccurenceDict.get))
+                popVal = EEZoccurenceDict.pop(max(EEZoccurenceDict, key=EEZoccurenceDict.get))
+            # Count instances of particles at EEZ in target list at a certain timestep
+            def EEZ_counter(timestep):
+                currEEZ = np.where(np.isnan(pfile['EEZ'].data[timestep]), -1, pfile['EEZ'].data[timestep])
+                currCount = np.zeros(len(plotEEZbars))
+                for el in currEEZ:
+                    try:
+                        idx = plotEEZbars.index(el)
+                        currCount[idx] += 1
+                    except ValueError:
+                        pass
+                return currCount
 
-        # Specify EEZs for bar chart
-        targets = [0, 210, 218, 223] # -1 for NaN
-        # Count instances of particles at EEZ in target list at a certain timestep
-        def EEZ_counter(timestep):
-            currEEZ = np.where(np.isnan(pfile['EEZ'].data[timestep]), -1, pfile['EEZ'].data[timestep])
-            currCount = np.zeros(len(targets))
-            for el in currEEZ:
-                try:
-                    idx = targets.index(el)
-                    currCount[idx] += 1
-                except ValueError:
-                    pass
-            return currCount
+            divider = make_axes_locatable(particle_map)
+        
+            barplot = divider.append_axes("right", size=2, pad=1, axes_class=plt.Axes)
 
-        divider = make_axes_locatable(particle_map)
-        barplot = divider.append_axes("right", size=2, pad=1, axes_class=plt.Axes)
-
-        bar    = barplot.barh(np.arange(len(targets)), EEZ_counter(currtime))
-        barplot.set_xlim(0, 40)
-        barplot.set_yticks(np.arange(len(targets)))
-        barplot.set_yticklabels(['No EEZ' if label == -1 else str(label) for label in targets])
-        barplot.set_ylabel('EEZ ID')
-        barplot.set_xlabel('Count')
-        barplot.invert_yaxis()
-        titel = fig.suptitle('Particles at time ' + str(plottimes[0])[:13])
+            bar    = barplot.barh(np.arange(nbar), EEZ_counter(currtime))
+            barplot.set_xlim(0, barLength)
+            barplot.set_yticks(np.arange(nbar))
+            if eezIDmap is not None:
+                EEZ_df = pickle.load(eezIDmap)['df']
+                barplot.set_yticklabels([EEZ_df[EEZ_df['ID'] == ID]['ISO'].values[0] for ID in plotEEZbars])
+            else: 
+                barplot.set_yticklabels([str(label) for label in plotEEZbars])
+            barplot.set_ylabel('EEZ ID')
+            barplot.set_xlabel('Count')
+            barplot.invert_yaxis()
+        
+        title = fig.suptitle('Particles at time ' + str(plottimes[0])[:13])
         frames = np.arange(0, len(plottimes))
 
         def animate(t):
             currtime = time == plottimes[t]
             scat.set_offsets(np.vstack((lon[currtime], lat[currtime])).transpose())
-            titel.set_text('Particles in different EEZs at time ' + str(plottimes[t])[:13])
+            title.set_text('Particles in different EEZs at time ' + str(plottimes[t])[:13])
             for rect, width in zip(bar, EEZ_counter(currtime)):
                 rect.set_width(width)
             return scat,
-        anim = animation.FuncAnimation(fig, animate, interval=20, blit=False)  #Frames, init_func=init
+        anim = animation.FuncAnimation(fig, animate, frames=len(plottimes), blit=False)
         anim.save('particle_EEZ_evo.mp4', fps=fps, metadata={'artist':'Daan', 'title':'Particles in EEZs'}, extra_args=['-vcodec', 'libx264'])
 
         plt.show()
