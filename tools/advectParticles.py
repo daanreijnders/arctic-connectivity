f2202f21639b1aa8ae577a742a4c0caddc3349d2
Reijnders <b.j.h.r.reijnders@uu.nl>
2019-07-15 19:16:56 +0200

    removed countgrid from advectParticles

--- tools/advectParticles.py
+++ tools/advectParticles.py
@@ -8,14 +8,13 @@ from datetime import datetime
 import sys
 import argparse
 
-
-
 from parcels import (grid, Field, FieldSet, ParticleSet, JITParticle, ScipyParticle, AdvectionRK4,
                      ErrorCode, ParticleFile, Variable, plotTrajectoriesFile)
 
 sys.path.append('/home/students/4302001/arctic-connectivity/tools')
 sys.path.append('/Users/daanreijnders/surfdrive/Thesis/repo/tools')
 import comtools
+import fieldsetter
 
 # Specify paths to velocity field and mesh
 # readdir_ocean = '/Users/daanreijnders/Datasets/'
@@ -32,33 +31,6 @@ meshfile = 'POP_grid_coordinates.nc'
 
 writedir = '/scratch/DaanR/psets/'
 
-def read_velocity_field(fieldfiles, meshfile=None):
-    if not meshfile:
-        meshfile = fieldfiles
-    filenames = {'U': {'lon': meshfile,
-                       'lat': meshfile,
-                       'data':fieldfiles},
-                 'V': {'lon': meshfile,
-                       'lat': meshfile,
-                       'data':fieldfiles}}
-
-    variables = {'U': 'UVEL_5m',
-                 'V': 'VVEL_5m'}
-
-    dimensions = {'U': {'time': 'time',
-                        'lat': 'ULAT',
-                        'lon': 'ULON'},
-                  'V': {'time': 'time',
-                        'lat': 'ULAT',
-                        'lon': 'ULON'}}
-    fieldset = FieldSet.from_pop(filenames, variables, dimensions, allow_time_extrapolation=False)
-    fieldset.U.vmax = 10;  fieldset.U.vmin = -10;  # set max of flow to 10 m/s
-    fieldset.V.vmax = 10; fieldset.V.vmin = -10;
-    
-    fieldset.computeTimeChunk(fieldset.U.grid.time[0], 1)
-    fieldset.landMask = np.logical_or(fieldset.U.data[0,:,:]==-0.01, np.abs(fieldset.U.data[0,:,:])<0.0000001)
-    return fieldset
-
 # Kernels for circular boundary and for deleting particles as fallback.
 def wrapLon(particle, fieldset, time):
     if particle.lon > 180.:
@@ -71,7 +43,7 @@ def deleteParticle(particle, fieldset, time):
     
 # Particle execution function
 def gridAdvection(fieldset, \
-                  countGrid, \
+#                   countGrid, \
                   particleGrid, \
                   experiment_name='', \
                   runtime=delta(days=30), \
@@ -89,15 +61,20 @@ def gridAdvection(fieldset, \
     return pset
 
 if __name__ == '__main__':
-    fieldset = read_velocity_field(readdir_ocean+fieldfile_ocean, meshfile=readdir_mesh+meshfile)  
+    fieldset = fieldsetter.read_velocity_field(readdir_ocean+fieldfile_ocean, meshfile=readdir_mesh+meshfile)  
     parser = argparse.ArgumentParser(description="Advect particles on a rectilinear grid.")
     parser.add_argument('plon', type=int, help='Number of particles spaced over longitudes.')
     parser.add_argument('plat', type=int, help='Number of particles spaced over latitudes.')
-    parser.add_argument('clon', type=int, help='Number of counting intervals in longitudinal direction.')
-    parser.add_argument('clat', type=int, help='Number of counting intervals in latitudinal direction.')
+#     parser.add_argument('clon', type=int, help='Number of counting intervals in longitudinal direction.')
+#     parser.add_argument('clat', type=int, help='Number of counting intervals in latitudinal direction.')
     parser.add_argument('-n', '--name', default='', type=str, help='Experiment name to save pset with.')
-    parser.add_argument('-mlat', '--minimumlat', default=60, type=str, help='Experiment name to save pset with.')
+    parser.add_argument('-mlat', '--minimumlat', default=60, type=str, help='minimumlat')
     args = parser.parse_args()
-    countG = comtools.countGrid(args.clon, args.clat)
+    if args.name:
+        name = args.name
+    else:
+        name = ''
+#    countG = comtools.countGrid(args.clon, args.clat)
     particleG = comtools.particleGrid(args.plon, args.plat, 0)
-    psetTest = gridAdvection(fieldset, countG, particleG, experiment_name='')
+#     psetTest = gridAdvection(fieldset, countG, particleG, experiment_name=f"{name}_P{args.plon}x{args.plat}")
+    psetTest = gridAdvection(fieldset, particleG, experiment_name=f"{name}_P{args.plon}x{args.plat}")
