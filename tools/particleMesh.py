bb498001a46e70081c6f91ab6f7bb472494c5ef4
Daan Reijnders <daanreijnders@outlook.com>
2019-10-08 16:57:47 +0200

    added stripy parts

--- tools/particleMesh.py
+++ tools/particleMesh.py
@@ -13,6 +13,3 @@ def save_ico_mesh_particles(refinement):
     icoMesh['lons'] = ico.lons
     with open('ico_mesh_parts_r{}_noF'.format(refinement), 'wb') as dumpFile:
         pickle.dump(icoMesh, dumpFile)
-save_ico_mesh(10)
-#save_ico_mesh(11)
-#save_ico_mesh(12)
