06e3768f650d38b137b08a48b48890193536e1c6
Reijnders <b.j.h.r.reijnders@uu.nl>
2019-10-14 23:05:04 +0200

    added parcels' implementation of antibeaching

--- diagnostics/particle_animation.py
+++ diagnostics/particle_animation.py
@@ -26,7 +26,7 @@ fieldfile_ice = 'monthly_icefields_CESM_0.1degree_controlrun_year_300_arctic.nc'
 meshfile = 'POP_grid_coordinates.nc'
 
 writedir = '/scratch/DaanR/psets/'
-
+psetdir = '/data/oceanparcels/output_data/data_Daan/psets/'
 # timestamps = [[np.datetime64('2000-01-09', 'D') + np.timedelta64(day, 'D') for day in range(4007)]]
 # fieldset = fieldsetter.read_velocity_field(readdir_ocean+fieldfile_ocean, 
 #                                            meshfile = readdir_mesh+meshfile,
@@ -35,9 +35,8 @@ writedir = '/scratch/DaanR/psets/'
 
 #pfile = xr.open_dataset('/data/oceanparcels/output_data/data_Daan/psets/pset_Rrcp85_Piconorth11_S2000-01-9_D90_DT20_ODT24_LAT60-90_LON-180-180.nc', decode_cf=True)
 # pfile = xr.open_dataset('/data/oceanparcels/output_data/data_Daan/psets/pset_Rcontrol_y300_DT5C_P3590x590_S2000-1-1_D30_DT5_ODT12_LAT60.5-89.5_LON-179.5-179.5.nc', decode_cf=True)
+psetname = 'pset_Rcontroly300_Piconorth11_S2000-01-1_D90_DT20_ODT24_LAT60-90_LON-180-180.nc'
 
-pfile = xr.open_dataset('/data/oceanparcels/output_data/data_Daan/psets/pset_Rrcp85_P3600x600_S2000-1-9_D90_DT20_ODT12_LAT60.1-89.9_LON-179.9-179.9.nc', decode_cf=True)
-
-
-plot.particleAnimation.create(pfile, particle_subsample=5, times='flat', fps=6, extent=(-180,180,60,90), polar=True, mask=False, titleAttribute='pset_Rrcp85_P3600x600_S2000-1-9_D90_DT20_ODT12_LAT60.1-89.9_LON-179.9-179.9.nc')
+pfile = xr.open_dataset(psetdir + psetname, decode_cf=True)
+plot.particleAnimation.create(pfile, particle_subsample=5, times='flat', fps=6, extent=(-180,180,60,90), polar=True, mask=False, titleAttribute=psetname)
 pfile.close()
--- tools/kernelCollection.py
+++ tools/kernelCollection.py
@@ -1,16 +1,40 @@
+from parcels import (JITParticle, Variable)
+import numpy as np
+
+class unbeachableParticle(JITParticle):
+        # beached : 0 sea, 1 beached, 2 after non-beach dyn, 3 after beach dyn, 4 please unbeach
+        beached = Variable('beached', dtype=np.int32, initial=0.)
+        unbeachCount = Variable('unbeachCount', dtype=np.int32, initial=0.)   
+        
 # Kernels for circular boundary and for deleting particles as fallback.
 def wrapLon(particle, fieldset, time):
     if particle.lon > 180.:
         particle.lon = particle.lon - 360.
     if particle.lon < -180.:
         particle.lon = particle.lon + 360.
+        
+def advectionRK4(particle, fieldset, time):
+    if particle.beached == 0:        
+        (u1, v1) = fieldset.UV[time, particle.depth, particle.lat, particle.lon]
+        lon1, lat1 = (particle.lon + u1*.5*particle.dt, particle.lat + v1*.5*particle.dt)
 
+        (u2, v2) = fieldset.UV[time + .5 * particle.dt, particle.depth, lat1, lon1]
+        lon2, lat2 = (particle.lon + u2*.5*particle.dt, particle.lat + v2*.5*particle.dt)
+
+        (u3, v3) = fieldset.UV[time + .5 * particle.dt, particle.depth, lat2, lon2]
+        lon3, lat3 = (particle.lon + u3*particle.dt, particle.lat + v3*particle.dt)
+
+        (u4, v4) = fieldset.UV[time + particle.dt, particle.depth, lat3, lon3]
+        particle.lon += (u1 + 2*u2 + 2*u3 + u4) / 6. * particle.dt
+        particle.lat += (v1 + 2*v2 + 2*v3 + v4) / 6. * particle.dt
+        particle.beached = 2
+        
 def deleteParticle(particle, fieldset, time):
+    print(f"Particle {particle.id} deleted: ({particle.lon}, {particle.lat} at {particle.time})")
     particle.delete()
     
-    
-# From
-def BeachTesting_2D(particle, fieldset, time):
+# From https://github.com/OceanParcels/Parcelsv2.0PaperNorthSeaScripts        
+def beachTesting(particle, fieldset, time):
     if particle.beached == 2 or particle.beached == 3:
         (u, v) = fieldset.UV[time, particle.depth, particle.lat, particle.lon]
         if fabs(u) < 1e-14 and fabs(v) < 1e-14:
@@ -21,7 +45,7 @@ def BeachTesting_2D(particle, fieldset, time):
         else:
             particle.beached = 0
 
-def UnBeaching(particle, fieldset, time):
+def unBeaching(particle, fieldset, time):
     if particle.beached == 4:
         (ub, vb) = fieldset.UVunbeach[time, particle.depth, particle.lat, particle.lon]
         particle.lon += ub * particle.dt
